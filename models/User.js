91e6969f7cbfeb806d298b399e40bacdb438c407
GiordanoSouza <giordano.souza@engenharia.ufjf.br>
2024-11-03 10:02:31 -0300

    primeiro commit

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');  
+const { Schema } = mongoose;	
+const bcrypt = require('bcrypt');
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        unique: true
+    
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre("save", async function(next) {
+    const user = this;
+    if(!user.isModified('password')) return next();
+    let salt = await bcrypt.genSalt(10);
+    let hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+    next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
