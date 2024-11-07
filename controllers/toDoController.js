695d83e03a34f5c4fba2a06cfc4976ccaa4f54bf
GiordanoSouza <giordanowrk@gmail.com>
2024-11-07 19:40:47 -0300

    Finalizarmos vídeo 18, testamos todas as rotas e caminhos para o banco de dados

--- controllers/toDoController.js
+++ controllers/toDoController.js
@@ -12,3 +12,43 @@ exports.createToDo = async (req, res) => {
         res.status(err);
     }
 }
+
+exports.getAllToDo = async (req, res) => {
+    let{userId}= req.params;
+    try{
+        const result = await ToDo.find({createdBy: userId});
+        res.send(result);
+    }catch(err){
+        console.log('Erro ao buscar ToDos:', err);
+        res.status(400).send(err);
+    }
+}
+
+
+
+exports.updateToDo = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const data = req.body;
+        const result = await ToDo.findByIdAndUpdate(id, {$set:data},{returnOriginal: false});
+        console.log(result);
+        res.send({message: 'ToDo updated, ligma'});
+    }catch(err){
+        console.log(err)
+        res.status(400).send(err)
+    }
+}
+
+
+exports.deleteToDo = async (req, res) => {
+    try{
+        const {id}=req.params
+        const result = await ToDo.findByIdAndDelete(id);
+        console.log(result);
+        res.send({message: 'ToDo deleted successfully'});
+
+    }catch(err){
+        console.log('Erro ao deletar ToDo:', err);
+        res.status(400).send(err);
+    }
+}
