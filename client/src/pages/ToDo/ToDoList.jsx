c268545c1baba6ad1a587d9c31905889c3582ae1
GiordanoSouza <giordanowrk@gmail.com>
2024-11-07 21:05:43 -0300

    Finalização vídeo 20, adicionando a task para criar uma nova tarefa, "creation models"

--- client/src/pages/ToDo/ToDoList.jsx
+++ client/src/pages/ToDo/ToDoList.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from '../../components/Navbar'
+import styles from './ToDoList.module.css'
+import {Button, Divider, Input, Modal, message} from 'antd'
+import ToDoServices from '../../services/toDoServices';
+import {getErrorMessage} from '../../util/GetError'
+import { getUserDetails } from '../../util/GetUser';
 
 function ToDoList() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmitTask = async () => {
+    setLoading(true);
+    try{
+      const userId = getUserDetails()?.userId;
+      const data = {
+        title,
+        description,
+        isCompleted: false,
+        userId:userId,
+      }
+      const response = await ToDoServices.createToDo(data);
+      console.log(response.data);
+      setLoading(false);
+      message.success('Task added successfully');
+      setIsAdding(false);
+    }catch(err){
+      console.log(err);
+      setLoading(false);
+      message.error(getErrorMessage(err));
+    }
+    
+  }
   return (
-    <Navbar active={"myTask"}/>
+    <>
+      <Navbar activate={"MyTask"} />
+      <section className={styles.toDoWrapper}>
+        <div className={styles.toDoHeader}>
+          <h2>Your Tasks</h2>
+          <Input style={{ width: '50%' }} placeholder='search your task here' />
+          <div>
+            <Button onClick={()=>setIsAdding(true)} type="primary" size="large">Add Task</Button>
+          </div>
+        </div>
+        <Divider />
+        <Modal
+          confirmLoading={loading}
+          title="Add New To Do Task"
+          open={isAdding}
+          onOk={handleSubmitTask}
+          onCancel={() => setIsAdding(false)}
+        >
+          <Input
+            style={{ marginBottom: '1rem' }}
+            placeholder='Title'
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Input.TextArea
+            placeholder='Description'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Modal>
+      </section>
+    </>
   )
 }
 
--- client/src/services/toDoServices.js
+++ client/src/services/toDoServices.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import { getUserDetails } from '../util/GetUser';
+
 
 const SERVER_URL = 'http://localhost:5000/api/todo';
 
 const authHeaders = () => {
-    let userToken = getUseDetails()?.token;
+    let userToken = getUserDetails()?.token;
     return {
         headers: {'Authorization':userToken}
 }
